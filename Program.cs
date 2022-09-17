813fe190b86654705ff081896e204710e158983b
Андрей Охотников <andrey1q2q@gmail.com>
2022-09-17 21:43:00 +1000

    Сделал программу которая принимает на вход значения и и сохраняет их в массив если длинна значения меньше или равна 3 символам

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.Write("Введите данные разделяя их пробелом: ");
+string input = Console.ReadLine()!;
+
+string[] values = input.Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
+
+string[] filteredValues = new string[0];
+
+foreach (string value in values)
+{
+    if (value.Length < 4)
+        filteredValues = FillArray(filteredValues, value);
+}
+
+PrintArray(values, "Изначальный массив");
+PrintArray(filteredValues, "Отфильтрованный массив");
+
+void PrintArray(string[] array, string message)
+{
+    Console.Write($"{message} {String.Join(", ", array)}");
+    Console.WriteLine();
+}
+
+string[] FillArray(string[] array, string value)
+{
+    int arraySize = array.Length;
+    Array.Resize(ref array, arraySize + 1);
+    array[arraySize] = value;
+    return array;
+}
